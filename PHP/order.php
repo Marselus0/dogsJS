380ebc0896f7a01b20a2724d222b92407212fe33
Marselus0 <manya93.93@mail.ru>
2023-08-23 16:59:22 +0300

    link

---
+++ PHP/order.php
@@ -0,0 +1,40 @@
+<?php
+
+$headers = 'Content-type: text/plain; charset="utf-8"';
+
+$username = $_POST['username'];
+$usermail = $_POST['usermail'];
+$userphone = $_POST['userphone'];
+$usertelegram = $_POST['usertelegram'];
+
+$headers = 'MiME-Version: 1.0' . "\r\n";
+$headers = 'Content-type: text/plain; charset="utf-8"';
+
+$to = 'email@mail.ru';
+$subject = 'Заявка с сайта';
+$message = "
+<html>
+    <table>
+        <tr>
+            <td>Имя</td>
+            <td>$username</td>
+        </tr>
+        <tr>
+            <td>Email</td>
+            <td>$usermail</td>
+        </tr>
+        <tr>
+            <td>Телефон</td>
+            <td>$userphone</td>
+        </tr>
+        <tr>
+            <td>Телеграм</td>
+            <td>$usertelegram</td>
+        </tr>
+    </table>
+</html>
+";
+
+mail($to, $subject, $message, $headers);
+
+?>
